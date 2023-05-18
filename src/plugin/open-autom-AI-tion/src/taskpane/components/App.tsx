a71faf85c1ab6e9b29abc6a1cb48ba36321bf904
Oliver <seitz-oliver@gmx.de>
2023-05-18 18:14:55 +0200

    plugin development

--- src/plugin/open-autom-AI-tion/src/taskpane/components/App.tsx
+++ src/plugin/open-autom-AI-tion/src/taskpane/components/App.tsx
@@ -1,78 +1,179 @@
 import * as React from "react";
-import { DefaultButton } from "@fluentui/react";
-import Header from "./Header";
-import HeroList, { HeroListItem } from "./HeroList";
+import { DefaultButton, TextField } from "@fluentui/react";
 import Progress from "./Progress";
+import { useCallback, useEffect } from "react";
+import { KeyboardEvent } from "react";
+import { useState } from "react";
+import config from "../../config/config";
 
-/* global require */
+async function queryOpenAi(messages: { role: string, content: string }[]) {
+  const { endpoint, key, model } = config;
 
-export interface AppProps {
-  title: string;
-  isOfficeInitialized: boolean;
-}
+  const url = `${endpoint}openai/deployments/${model}/chat/completions?api-version=2023-03-15-preview`;
 
-export interface AppState {
-  listItems: HeroListItem[];
-}
-
-export default class App extends React.Component<AppProps, AppState> {
-  constructor(props, context) {
-    super(props, context);
-    this.state = {
-      listItems: [],
-    };
-  }
-
-  componentDidMount() {
-    this.setState({
-      listItems: [
-        {
-          icon: "Ribbon",
-          primaryText: "Achieve more with Office integration",
-        },
-        {
-          icon: "Unlock",
-          primaryText: "Unlock features and functionality",
-        },
-        {
-          icon: "Design",
-          primaryText: "Create and visualize like a pro",
-        },
-      ],
-    });
-  }
-
-  click = async () => {
-    /**
-     * Insert your Outlook code here
-     */
+  const body = {
+    messages: messages,
+    max_tokens: 800,
+    temperature: 0.7,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    top_p: .95,
+    stop: null
   };
 
-  render() {
-    const { title, isOfficeInitialized } = this.props;
+  console.log(url);
 
-    if (!isOfficeInitialized) {
-      return (
-        <Progress
-          title={title}
-          logo={require("./../../../assets/logo-filled.png")}
-          message="Please sideload your addin to see app body."
-        />
-      );
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "api-key": key
+    },
+    body: JSON.stringify(body)
+  });
+
+  if (!response.ok) {
+    return null;
+  }
+
+  return await response.json();
+}
+
+function formatMessageForDisplay(content: string) {
+  const marker = "***";
+  const parts = [];
+
+  let rest = content;
+  let index = rest.indexOf(marker);
+  let format = index === 0 ? "mail" : "chat";
+  if (index === 0) {
+    rest = rest.substring(index + 3);
+    index = rest.indexOf(marker);
+  }
+
+  while (index > -1) {
+    const part = rest.substring(0, index);
+
+    parts.push({
+      format: format,
+      content: part
+    });
+
+    format = format === "mail" ? "chat" : "mail";
+
+    rest = rest.substring(index + 3);
+    index = rest.indexOf(marker);
+  }
+
+  parts.push({
+    format: format,
+    content: rest
+  });
+
+  return parts.map(p =>
+    <p data-format={p.format}>
+      {p.content}
+    </p>
+  );
+}
+
+export default (props) => {
+  const { title, isOfficeInitialized } = props;
+
+  const [loading, setLoading] = useState(false);
+  const [query, setQuery] = useState("");
+  const [messages, setMessages] = useState([
+    {
+      role: "system",
+      content: "You are an virtual assistant. You help the user named Oliver with his emails. Use a positive, encouraging tone."
+    },
+    {
+      role: "system",
+      content: "If you provide the content of a draft email, enclose it with three stars."
     }
+  ]);
 
+  // Debug Hook
+  useEffect(() => console.table(messages), [messages]);
+
+  useEffect(() => {
+    let exited = false;
+
+    console.log("Get content of email.")
+
+    Office.context.mailbox.item?.body.getAsync(Office.CoercionType.Text,
+      (data: Office.AsyncResult<string>) => {
+        console.log("Got content of email.");
+
+        if (exited) return;
+
+        const message = {
+          role: "system",
+          content: `The current email he needs help with has the following content.\n\n###\n${data.value}\n###`
+        }
+
+        setMessages(m => [...m, message]);
+      });
+
+    return () => { exited = true; };
+  }, []);
+
+  const onKeyDown = useCallback(async (e: KeyboardEvent) => {
+    if (e.code === 'Enter') {
+      setLoading(true);
+
+      const requestMessage = {
+        role: "user",
+        content: query
+      };
+
+      setMessages(m => [...m, requestMessage]);
+
+      const response = await queryOpenAi([...messages, requestMessage]);
+      const responseMessage = response.choices[0].message;
+      setMessages(m => [...m, responseMessage]);
+
+      setQuery("");
+      setLoading(false);
+    }
+  }, [query, messages]);
+
+  const onChange = useCallback((_: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => {
+    setQuery(newValue)
+  }, [setQuery]);
+
+  if (!isOfficeInitialized) {
     return (
-      <div className="ms-welcome">
-        <Header logo={require("./../../../assets/logo-filled.png")} title={this.props.title} message="Welcome" />
-        <HeroList message="Discover what Office Add-ins can do for you today!" items={this.state.listItems}>
-          <p className="ms-font-l">
-            Modify the source files, then click <b>Run</b>.
-          </p>
-          <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronRight" }} onClick={this.click}>
-            Run
-          </DefaultButton>
-        </HeroList>
-      </div>
+      <Progress
+        title={title}
+        logo={require("./../../../assets/logo-filled.png")}
+        message="Please sideload your addin to see app body."
+      />
     );
   }
-}
+
+  return (
+    <div>
+      <TextField className="query-input" placeholder="Ask me anything" onKeyDown={onKeyDown} onChange={onChange} value={query}></TextField>
+      <div className="message-list">
+        {
+          messages
+            .filter(o => o.role != "system")
+            .map(o =>
+              <div data-role={o.role} className="message-bubble">
+                {formatMessageForDisplay(o.content)}
+              </div>
+            )
+        }
+
+        {
+          loading ?
+            <div data-role="assistant" className="message-bubble">
+              {"Thinking ..."}
+            </div>
+            : null
+        }
+      </div>
+    </div>
+  );
+};
